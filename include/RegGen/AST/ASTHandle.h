71bdc76c522217d64e06df2ac0a43fe14533e1f7
CanftIn <wwc7033@gmail.com>
2023-06-15 00:52:00 +0800

    update ast handler

--- include/RegGen/AST/ASTHandle.h
+++ include/RegGen/AST/ASTHandle.h
@@ -64,6 +64,87 @@ class ASTItemSelector {
   int index_;
 };
 
+class ASTManipPlaceholder {
+ public:
+  void Invoke(const ASTTypeProxy& proxy, ASTItem item,
+              ArrayRef<ASTItem> rhs) const {}
+};
+
+class ASTObjectSetter {
+ public:
+  struct SetterPair {
+    int member_index;
+    int symbol_index;
+  };
+
+  explicit ASTObjectSetter(const SmallVector<SetterPair>& setters)
+      : setters_(setters) {}
+
+  void Invoke(const ASTTypeProxy& proxy, ASTItem obj,
+              ArrayRef<ASTItem> rhs) const {
+    for (auto setter : setters_) {
+      proxy.AssignField(obj, setter.member_index, rhs[setter.symbol_index]);
+    }
+  }
+
+ private:
+  SmallVector<SetterPair> setters_;
+};
+
+class ASTVectorMerger {
+ public:
+  explicit ASTVectorMerger(const SmallVector<int>& indices)
+      : indices_(indices) {}
+
+  void Invoke(const ASTTypeProxy& proxy, ASTItem vec,
+              ArrayRef<ASTItem> rhs) const {
+    for (auto index : indices_) {
+      proxy.PushBackElement(vec, rhs[index]);
+    }
+  }
+
+ private:
+  SmallVector<int> indices_;
+};
+
+class ASTHandle {
+ public:
+  using GenHandle = std::variant<ASTEnumGen, ASTObjectGen, ASTVectorGen,
+                                 ASTOptionalGen, ASTItemSelector>;
+
+  using ManipHandle =
+      std::variant<ASTManipPlaceholder, ASTObjectSetter, ASTVectorMerger>;
+
+  ASTHandle(const ASTTypeProxy* proxy, GenHandle gen, ManipHandle manip)
+      : proxy_(proxy), gen_handle_(gen), manip_handle_(manip) {}
+
+  auto Invoke(Arena& arena, ArrayRef<ASTItem> rhs) const -> ASTItem {
+    auto gen_visitor = [&](const auto& gen) {
+      return gen.Invoke(*proxy_, arena, rhs);
+    };
+    auto result = std::visit(gen_visitor, gen_handle_);
+
+    auto manip_visitor = [&](const auto& manip) {
+      manip.Invoke(*proxy_, result, rhs);
+    };
+    std::visit(manip_visitor, manip_handle_);
+
+    auto front_loc = const_cast<ASTItem&>(rhs.front()).GetLocationInfo();
+    auto back_loc = const_cast<ASTItem&>(rhs.back()).GetLocationInfo();
+
+    auto offset = front_loc.offset;
+    auto length = back_loc.offset + back_loc.length - offset;
+    result.UpdateLocationInfo(offset, length);
+
+    return result;
+  }
+
+ private:
+  const ASTTypeProxy* proxy_;
+
+  GenHandle gen_handle_;
+  ManipHandle manip_handle_;
+};
 
 }  // namespace RG::AST
 
